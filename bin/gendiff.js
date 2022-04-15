c601bb541320561fc32115d6ec744beda51b1794
DenisFrolkin <denisfrolkin@gmail.com>
2022-04-15 20:17:23 +0300

    fix arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const program = new Command();
 program
 .version('1.0.0', '-V', '--version')
 .description('Compares two configuration files and shows a difference.')
-.arguments('<filepath1>', '<filepath2>')
+.arguments('<filepath1> <filepath2>')
 .option('-f, --FormData<type>')
 
 program.parse(process.argv);

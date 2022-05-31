cc3c1657f675d035de9a9610a70c49aa6004e70d
DenisFrolkin <denisfrolkin@gmail.com>
2022-06-01 01:11:45 +0300

    step 3 fixing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
-    .argument('<filepath1> <filepath2>')
+    .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
     .action((filepath1, filepath2) => gendiff(filepath1, filepath2));
 
--- src/index.js
+++ src/index.js
@@ -6,46 +6,54 @@ import _ from 'lodash';
 
     const gendiff = (filepath1, filepath2) => {
       const obj1 = JSON.parse(readFileSync(path.resolve(process.cwd(), filepath1)));
+      console.log(obj1);
       const obj2 = JSON.parse(readFileSync(path.resolve(process.cwd(), filepath2)));
-      const keys = Object.keys({...obj1, ...obj2});
+      console.log(obj2);
+
+      
+      const keys = _.sortBy(Object.keys({...obj1, ...obj2}));
+      console.log(keys)
       const newData = keys.map((key) => {
         const res = {};
         if (obj1[key] === obj2[key]) {
-          res[newKey] = key;
-          res[value1] = obj1[key];
-          res[value2] = obj2[key];
+          res.newKey = key;
+          res.value1 = obj1[key];
+          res.value2 = obj2[key];
           return res;
         }
           else if (obj1[key] && obj2[key]) {
-          res[newKey] = key;
-          res[value1] = obj1[key];
-          res[value2] = obj2[key];
+          res.newKey = key;
+          res.value1 = obj1[key];
+          res.value2 = obj2[key];
           return res;
         } 
           else if (!obj2[key]) {
-          res[newKey] = key;
-          res[value1] = obj1[key];
+          res.newKey = key;
+          res.value1 = obj1[key];
           return res;
         } else {
-          res[newkey] = key;
-          res[value2] = obj2[key];
+          res.newKey = key;
+          res.value2 = obj2[key];
           return res;
         }
       });
+console.log(newData)
+
+
       console.log('{')
       for (const obj of newData) {
-        if (obj[value1] === obj[value2]) {
-          console.log('   ${obj[newKey]}: ${obj[value1]}');
+        if (obj.value1 === obj.value2) {
+          console.log(`   ${obj.newKey}: ${obj.value1}`);
         }
-        else if (obj[value1] && obj[value2]) {
-          console.log(' - ${obj[newKey]}: ${obj[value1]}');
-          console.log(' + ${obj[newKey]}: ${obj[value2]}');
+        else if (obj.value1 && obj.value2) {
+          console.log(` - ${obj.newKey}: ${obj.value1}`);
+          console.log(` + ${obj.newKey}: ${obj.value2}`);
         }
-        else if (!obj[value2]) {
-          console.log(' - ${obj[newKey]}: ${obj[value1]}');
+        else if (!obj.value2) {
+          console.log(` - ${obj.newKey}: ${obj.value1}`);
         }
         else {
-          console.log(' + ${obj[newKey]}: ${obj[value2]}');
+          console.log(` + ${obj.newKey}: ${obj.value2}`);
         }
       }
       console.log('}')

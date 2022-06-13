337059b908aa19b0360d30c750d22ee7053e42b2
DenisFrolkin <denisfrolkin@gmail.com>
2022-06-14 02:24:48 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,22 +1,33 @@
-import { fileURLToPath } from 'url';
+import path from 'path';
 import { dirname } from 'path';
+import { hasUncaughtExceptionCaptureCallback } from 'process';
+import { fileURLToPath } from 'url';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const correctResult = 
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+
+console.log(getFixturePath('file1.json'));
+console.log(typeof gendiff(getFixturePath('file1.json'), getFixturePath('file2.json')));
+
+
+
+const correctResult =
 `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-  }`   
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`;
 
 test('gendiff', () => {
-  expect(typeof gendiff('./__tests__/__fixtures__/file1.json', './__tests__/__fixtures__/file1.json')).toEqual('string');
   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(correctResult);
+});
+
+test('stringType', () => {
+  expect(typeof gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual('string');
 });

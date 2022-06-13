44135e032a455aad834b7bd68727201b8804a0ea
DenisFrolkin <denisfrolkin@gmail.com>
2022-06-14 02:01:07 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const correctResult = 
+`{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+  }`   
 
 test('gendiff', () => {
   expect(typeof gendiff('./__tests__/__fixtures__/file1.json', './__tests__/__fixtures__/file1.json')).toEqual('string');
+  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(correctResult);
 });

dbad79d57e9610cc5bc963dd86c05934597e1f2b
DenisFrolkin <denisfrolkin@gmail.com>
2022-08-02 23:34:48 +0300

    fixing tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,12 +12,6 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const experiment = (filename) => path.resolve(process.cwd(), '__tests__', '__fixtures__', filename);
 
-console.log(__filename);
-console.log(__dirname);
-console.log(process.cwd());
-console.log(path.resolve(process.cwd(), '__tests__', '__fixtures__', 'file1.json'));
-console.log(getFixturePath('file1.json'));
-
 const correctstylish = readFileSync(getFixturePath('correctStylish.yaml'), 'utf8');
 const correctplain = readFileSync(getFixturePath('correctPlain.yaml'), 'utf8');
 const correctJSON = readFileSync(getFixturePath('correctJSON.yaml'), 'utf8');

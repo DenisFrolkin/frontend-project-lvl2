35521de4b2569253b0b434b8e8a08ccaada02e75
DenisFrolkin <denisfrolkin@gmail.com>
2022-08-13 17:10:19 +0300

    fixing parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,16 @@
 import yaml from 'js-yaml';
 
 const parser = (data, ext) => {
-  if (ext === '.json') {
-    return JSON.parse(data);
-  } if (ext === '.yml' || ext === '.yaml') {
-    return yaml.load(data);
-  } return undefined;
+  switch (ext) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+      return yaml.load(data);
+    case '.yaml':
+      return yaml.load(data);
+    default:
+      throw Error(`${ext} - unknown type of file`);
+  }
 };
 
 export default parser;
